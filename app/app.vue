ca82db9d6e02eafcb1b61d06b2b61d5f20957dfc
Maxim Tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 11:32:42 +0200

    refactor(css): change CSS layer declaration for improved organization and clarity

--- app/app.vue
+++ app/app.vue
@@ -22,7 +22,7 @@ const sortedProjects = computed(() => {
 :root {
   --size-img-max-h: calc(100dvh - var(--size-base));
 }
-@layer base {
+@layer components {
   .app-wrap {
     display: grid;
     grid-template-columns: auto 1fr;
