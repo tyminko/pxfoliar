7d0007a3b4b9ac55702ab2a1cc0577c8d8092744
tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 14:25:13 +0000

    Deploying to gh-pages from @ tyminko/pxfoliar@8f68137020fbd52305c10f1113219f91e0e83796 🚀

--- _nuxt/C8GtDuMq.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,d as n,h as t,e as o}from"./9e7mqueA.js";const s={};function a(c,e){return o(),n("div",null,[...e[0]||(e[0]=[t("h1",null,"Artwork",-1)])])}const d=r(s,[["render",a]]);export{d as default};
--- _nuxt/Lpxu6EMO.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as o,e as r,Q as n}from"./9e7mqueA.js";const t={};function a(e,c){return r(),o("ol",null,[n(e.$slots,"default")])}const _=Object.assign(s(t,[["render",a]]),{__name:"ProseOl"});export{_ as default};
