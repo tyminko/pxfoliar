dfff0acdbac8dabdc377d71c75322030554ea393
tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 13:43:27 +0000

    Deploying to gh-pages from @ tyminko/pxfoliar@f03a305fdc252ba867a83747e12a8d96ce9ae463 🚀

---
+++ _nuxt/Y6hJsxQJ.js
@@ -0,0 +1,1 @@
+import{_ as o,d as t,e as s,Q as c}from"./D-Bpg-JV.js";const r={};function n(e,a){return s(),t("blockquote",null,[c(e.$slots,"default")])}const _=Object.assign(o(r,[["render",n]]),{__name:"ProseBlockquote"});export{_ as default};
