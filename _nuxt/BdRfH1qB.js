92175106472bf2ac7530ac8ab669121a04a2fd25
tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 09:08:56 +0000

    Deploying to gh-pages from @ tyminko/pxfoliar@79daa41e43ff329652a4abb6a2d96c8aa43cfa48 🚀

---
+++ _nuxt/BdRfH1qB.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as s,s as n}from"./LcvmmYMw.js";const t={};function c(e,a){return s(),r("strong",null,[n(e.$slots,"default")])}const l=Object.assign(o(t,[["render",c]]),{__name:"ProseStrong"});export{l as default};
