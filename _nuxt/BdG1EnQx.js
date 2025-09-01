788022c7ce36f2914ba4de336bc1bc101ba571a8
tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 13:48:26 +0000

    Deploying to gh-pages from @ tyminko/pxfoliar@e5d6c38e66976385595b7f94144fd4b69698770e 🚀

--- _nuxt/BdG1EnQx.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as a,e as r,Q as t}from"./D-Bpg-JV.js";const n={};function o(e,c){return r(),a("table",null,[t(e.$slots,"default")])}const _=Object.assign(s(n,[["render",o]]),{__name:"ProseTable"});export{_ as default};
--- _nuxt/CUrNOL2h.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,d as r,e as n,Q as o}from"./D-Bpg-JV.js";const t={};function a(e,c){return n(),r("li",null,[o(e.$slots,"default")])}const _=Object.assign(s(t,[["render",a]]),{__name:"ProseLi"});export{_ as default};
