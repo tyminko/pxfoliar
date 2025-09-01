48e3d85dedd26dbfc0e790b492187143f27f9536
Maxim Tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-01 11:48:02 +0200

    refactor(css): change CSS layer from components to base for improved organization

--- app/app.vue
+++ app/app.vue
@@ -22,7 +22,7 @@ const sortedProjects = computed(() => {
 :root {
   --size-img-max-h: calc(100dvh - var(--size-base));
 }
-@layer components {
+@layer base {
   .app-wrap {
     display: grid;
     grid-template-columns: auto 1fr;
