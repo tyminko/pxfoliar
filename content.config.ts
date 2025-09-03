5e0cf013ac0f38bd4d4318eb667cb6816b8a7d05
Maxim Tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-03 12:12:26 +0200

    update content configuration to change slug property to hohoho with hidden editor directive

--- content.config.ts
+++ content.config.ts
@@ -13,7 +13,7 @@ export default defineContentConfig({
           src: z.string().editor({ input: 'media' }),
           alt: z.string(),
         }),
-        slug: z.string().editor({ hidden: true }),
+        hohoho: z.string().editor({ hidden: true }),
         icon: z.string().optional().editor({ input: 'icon' }),
         authors: z.array(z.object({
           slug: z.string(),
