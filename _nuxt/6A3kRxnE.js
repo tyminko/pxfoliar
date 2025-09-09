d5523be41463fbc0e152a42a501221102c7e8667
tyminko <32540212+tyminko@users.noreply.github.com>
2025-09-09 06:21:40 +0000

    Deploying to gh-pages from @ tyminko/pxfoliar@9598523700d6daf2c8a3091d123c687278648f26 🚀

---
+++ _nuxt/6A3kRxnE.js
@@ -0,0 +1,1 @@
+import{_ as r,d as s,e as t,V as n}from"./BGKWWTvJ.js";const o={};function a(e,c){return t(),s("tr",null,[n(e.$slots,"default")])}const l=Object.assign(r(o,[["render",a]]),{__name:"ProseTr"});export{l as default};
---
+++ _nuxt/BfDlE8YN.js
@@ -0,0 +1,1 @@
+import{I as o,e as n,w as s,V as f,B as p}from"./BGKWWTvJ.js";const u={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const a=p;return n(),o(a,{href:t.href,target:t.target},{default:s(()=>[f(r.$slots,"default")]),_:3},8,["href","target"])}}};export{u as default};
